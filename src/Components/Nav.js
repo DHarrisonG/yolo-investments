b3ef8b27f136271df90c0d34cf5ced0dbf612119
DHarrisonG <dougharrisong@gmail.com>
2020-10-09 13:50:42 -0500

    Completed rough nav design and scheme

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -3,11 +3,16 @@ import React from 'react'
 class Nav extends React.Component {
     render() {
         return (
-            <div class="menubar">
-                <ul class="menulinks">
-                    <li>ABOUT</li>
-                    <li>CONTACT</li>
-                </ul>
+            <div className="menubar">
+                <div id="menu">
+                    <ul className="menulinks">
+                        <li>ABOUT</li>
+                        <li>CONTACT</li>
+                    </ul>
+                </div>
+                <div id="logo">
+                    <h1>Logo Temp</h1>
+                </div>
             </div>
         )
     }
