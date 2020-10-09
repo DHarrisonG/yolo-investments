1a6b5a9d224ea1b3492348d7afd67a17c6178705
DHarrisonG <dougharrisong@gmail.com>
2020-10-09 13:08:01 -0500

    Added Nav component, began setting up flexbox and initial links

---
+++ src/Components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Nav extends React.Component {
+    render() {
+        return (
+            <div class="menubar">
+                <ul class="menulinks">
+                    <li>ABOUT</li>
+                    <li>CONTACT</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Nav;
