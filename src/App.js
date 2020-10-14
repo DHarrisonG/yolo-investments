a692001c6668d62ccad042a1c058d61d925e0f54
DHarrisonG <dougharrisong@gmail.com>
2020-10-14 11:25:18 -0500

    Added company section including initial css styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 import Nav from './Components/Nav'
 import Header from './Components/Header'
+import Company from './Components/Company'
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Header />
+      <Company />
     </div>
   );
 }
---
+++ src/Components/Company.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Company extends React.Component {
+    render(){
+        return(
+            <div className='company-container'>
+                <h1>THE COMPANY</h1>
+                <hr></hr>
+                <h4>Dedicated to the old adage "Cash rules everything around us". We work hard to diversify your value so you don't have to.</h4>
+            </div>
+        )
+    }
+}
+
+export default Company;
