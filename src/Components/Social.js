9ea46fc1e63ddbe160c82c4f2d45ea582da22c98
DHarrisonG <dougharrisong@gmail.com>
2020-10-15 10:46:56 -0500

    Swapped social logos for white versions, linked them out, fixed some styling

--- src/Components/Social.js
+++ src/Components/Social.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
 class Social extends React.Component {
+
     render() {
         return (
             <div className='social-container'>
                 <h1>SOCIAL</h1>
                 <div className='social-links'>
-                    <img src="social-links/linkedin.png" />
-                    <img src="social-links/youtube.png" />
-                    <img src="social-links/twitter.png" />
-                    <img src="social-links/venmo.png" />
+                    <a href="https://www.linkedin.com/in/dharrisong" target="_blank"><img src="social-links/linkedin-white.png" /></a>
+                    <a href="https://www.youtube.com/watch?v=5IXQ6f6eMxQ&ab_channel=onlinefrog" target="_blank"><img src="social-links/youtube-white.png" /></a>
+                    <a href="https://twitter.com/coffee_dad?lang=en"><img src="social-links/twitter-white.png" target="blank" /></a>
+                    <a href="https://youtu.be/gHjronXyeqI?t=40" target="_blank"><img src="social-links/venmo-white.png" /></a>
                 </div>
             </div>
         )
